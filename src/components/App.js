a65eaf5584d6d4e9d3aabc013af8003f388eaaf0
Sean Chickosky <chickosky@gmail.com>
2020-07-23 18:13:22 -0700

    Cleaning up sign in UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../App.css';
-import { AmplifySignOut } from '@aws-amplify/ui-react';
+import { AmplifySignOut, AmplifyAuthenticator, AmplifySignIn } from '@aws-amplify/ui-react';
 import Views from './Views';
 import Upload from './Upload';
-import { ConfirmSignIn, ForgotPassword, RequireNewPassword, SignIn, VerifyContact, withAuthenticator } from 'aws-amplify-react';
+
 function App() {
   return (
     <div className="App container-fluid">
+      <AmplifyAuthenticator>
+      <div slot="sign-in"  className="align-items-center d-flex min-vh-100 justify-content-center">
+          <AmplifySignIn hideSignUp="true" />
+      </div>
       <Upload fleet="ra" />
       <Upload fleet="rse" />
       <Upload fleet="snoo" />
@@ -14,8 +18,9 @@ function App() {
       <Upload fleet="mirror" />
       <Views />
       <AmplifySignOut />
+      </AmplifyAuthenticator>
     </div>      
   );
 }
 
-export default withAuthenticator(App, false, [<SignIn />, <ConfirmSignIn />, <ForgotPassword />, <RequireNewPassword />, <VerifyContact />]);
+export default App;
